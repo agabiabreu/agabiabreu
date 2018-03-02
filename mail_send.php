88ea351432eadfd9b9a916453cc8a133d30b1ae2
agabiabreu <gabriela.abreu88@gmail.com>
2018-03-02 17:19:45 -0300

    teste

--- mail_send.php
+++ mail_send.php
@@ -1,18 +1,44 @@
 <?php
 
-// Coloque a mensagem que irá ser enviada para seu e-mail abaixo:
-$msg = "Mensagem enviada em ".date("d/m/Y").",
-os dados seguem abaixo:
-";
-while(list($campo, $valor) = each($HTTP_POST_VARS))
-{
-  $msg .= ucwords($campo).": ".$valor."
-";
-}
+	
+	//Variaveis de POST, Alterar somente se necessário 
+	//====================================================
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
 
-// Agora iremos fazer com que o PHP envie os dados do
-Formulário para seu e-mail: 
-mail("gabi@agabiabreu.com", "Assunto do
-E-mail",$msg,"From: $REMOTE_ADDR");
-echo "Seu e-mail foi enviado com sucesso. Obrigado";
+	//====================================================
+	
+	//REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
+	//==================================================== 
+	$email_remetente = "gabi@agabiabreu.com"; // deve ser uma conta de email do seu dominio 
+	//====================================================
+	
+	//Configurações do email, ajustar conforme necessidade
+	//==================================================== 
+	$email_destinatario = "gabi@agabiabreu.com"; // pode ser qualquer email que receberá as mensagens
+	$email_reply = "$email"; 
+	$email_assunto = "Contato formmail"; // Este será o assunto da mensagem
+	//====================================================
+	
+	//Monta o Corpo da Mensagem
+	//====================================================
+	$email_conteudo = "Nome = $nome \n"; 
+	$email_conteudo .= "Email = $email \n";
+
+	//====================================================
+	
+	//Seta os Headers (Alterar somente caso necessario) 
+	//==================================================== 
+	$email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
+	//====================================================
+	
+	//Enviando o email 
+	//==================================================== 
+	if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
+					echo "</b>E-Mail enviado com sucesso!</b>"; 
+					} 
+			else{ 
+					echo "</b>Falha no envio do E-Mail!</b>"; } 
+	//====================================================
+
 ?>
